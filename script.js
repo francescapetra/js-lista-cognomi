1e7165319d2c69b623929b73cc378f0e3cd70e43
francescapetra <francescalaurapetra@gmail.com>
2021-03-03 16:35:02 +0100

    esercizio svolto

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 var utente;
 
-var cognomi = [ "rossi", "bianchi", "verdi", "blu", "viola", "corallo" ];
-console.log(cognomi);
+var cognomi = [ "Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi" ];
+
+var lowercased = cognomi.map(name => name.toLowerCase());
 
 // Chiedi all'utente il cognome
  utente = prompt("inserisci il tuo cognome")
+ document.getElementById('posizione').innerHTML += utente;
+ // inseriscilo in un array di altri cognomi
+ lowercased.push(utente.toLowerCase());
 
-// inseriscilo in un array di altri cognomi
-
-
-
-// stampa la lista ordinata alfabeticamente
-
-
+ // stampa la lista ordinata alfabeticamente
+ lowercased.sort();
 
 // stampa la nuova posizione dove si trova l'utente
+
+for (var i = 0; i < lowercased.length; i++) {
+
+  if (utente == lowercased[i]) {
+    var umana = ++i;
+    // console.log(umana);
+    document.getElementById('scelto').innerHTML += umana ;
+  }
+}
+// console.log(lowercased);
+document.getElementById('lista_cognomi').innerHTML += "<li>" + lowercased + "</li>";
