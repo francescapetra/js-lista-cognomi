525bb83691230513caab04dce8085fcef1296f81
francescapetra <francescalaurapetra@gmail.com>
2021-03-03 14:23:26 +0100

    creazione index+script

---
+++ script.js
@@ -0,0 +1,17 @@
+var utente;
+
+var cognomi = [ "rossi", "bianchi", "verdi", "blu", "viola", "corallo" ];
+console.log(cognomi);
+
+// Chiedi all'utente il cognome
+ utente = prompt("inserisci il tuo cognome")
+
+// inseriscilo in un array di altri cognomi
+
+
+
+// stampa la lista ordinata alfabeticamente
+
+
+
+// stampa la nuova posizione dove si trova l'utente
